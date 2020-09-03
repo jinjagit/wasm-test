d2e7bdb619d76aa5a4728eea51056f4c9d58fb4e
Simon Tharby <jinjamail@gmail.com>
2020-09-03 15:11:51 +0100

    add js benchmark using Math.sin()

--- www/index.js
+++ www/index.js
@@ -1,4 +1,25 @@
 import * as wasm from "wasm-test";
 
-let result = wasm.times_two(21.3);
-console.log(result);
+let jsText = document.getElementById("js-result");
+
+let start = Date.now();
+
+var i;
+var result = 0;
+for (i = 1; i < 10000001; i++) {
+  result = result + Math.sin(i);
+} 
+
+let end = Date.now() - start;
+
+jsText.innerHTML = `Javascript only: ${end} ms, with final result = ${result}`;
+
+//start = Date.now();
+
+let res = wasm.times_two(21.3);
+
+//end = Date.now() - start;
+
+//console.log(end);
+
+console.log(res);
