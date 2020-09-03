fd1c65ed395f01cd636aafb516eb3d08d2134e26
Simon Tharby <jinjamail@gmail.com>
2020-09-03 23:20:34 +0100

    add comments

--- www/index.js
+++ www/index.js
@@ -9,6 +9,7 @@ const sineSeries = (n) => {
   return result;
 }
 
+// JS benchmark
 let jsText = document.getElementById("js-text");
 let start = Date.now();
 let result1 = sineSeries(10000000);
@@ -16,13 +17,15 @@ let t1 = Date.now() - start;
 
 jsText.innerHTML = `Javascript only: ${t1} ms, with final result = ${result1}`;
 
+// wasm benchmark
 let wasmText = document.getElementById("wasm-text");
 start = Date.now();
-let result2 = wasm.sine_series(10000000);
+let result2 = wasm.sine_series(10000000); // call wasm function
 let t2 = Date.now() - start;
 
 wasmText.innerHTML = `wasm called from JS: ${t2} ms, with final result = ${result2}`;
 
+// compare results
 let summaryText = document.getElementById("summary-text");
 
 if (t1 < t2) {
